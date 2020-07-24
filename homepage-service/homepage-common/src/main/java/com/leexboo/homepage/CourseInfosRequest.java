85a74a338a2214b8bd167b75ebbafd7a32b10de3
leexboo <386669232@qq.com>
2020-07-24 13:59:53 +0800

    commit

---
+++ homepage-service/homepage-common/src/main/java/com/leexboo/homepage/CourseInfosRequest.java
@@ -0,0 +1,15 @@
+package com.leexboo.homepage;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CourseInfosRequest {
+
+    private List<Long> ids;
+}
